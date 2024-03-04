72c2d5135118b6fcf60c70148c4f4893aa64b5a9
Kiril Kirilov <kiril.ivov.kirilov@gmail.com>
2024-03-04 14:07:17 +0200

    test

---
+++ src/ElidibleTowork.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ElidibleTowork {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age= sc.nextInt();
+
+        if (age >= 16) {
+            System.out.println("You are eligible to work.");
+        } else {
+            System.out.println("You are not eligible to work.");
+        }
+        }
+        }
+
